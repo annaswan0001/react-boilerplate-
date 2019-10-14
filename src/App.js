3204531949a762f39b8bd15745693ee2254db878
A.Kyzmenko <a.kyzmenko@MacBook-Air-Anna.local>
2019-10-14 16:38:23 +0400

    add prettier config

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React, { Component } from "react";
 import "./App.css";
-
 import { hot } from "react-hot-loader";
-import PropTypes from "prop-types";
 
 class App extends Component {
-  state = {
-    count: 0
-  };
+    state = {
+        count: 0
+    };
 
-  render() {
-    return (
-      <div>
-        {this.state.count}
-        <button
-          onClick={() => this.setState(state => ({ count: state.count + 1 }))}
-        >
-          +
-        </button>
-        <button
-          onClick={() => this.setState(state => ({ count: state.count - 1 }))}
-        >
-          -
-        </button>
-        <h1> hello Anna?</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {this.state.count}
+                <button
+                    onClick={() =>
+                        this.setState(state => ({
+                            count: state.count + 1
+                        }))
+                    }
+                >
+                    +
+                </button>
+                <button
+                    onClick={() =>
+                        this.setState(state => ({
+                            count: state.count - 1
+                        }))
+                    }
+                >
+                    -
+                </button>
+                <h1> hello Anna?</h1>
+                asd asd asd
+            </div>
+        );
+    }
 }
 
 App.propTypes = {};
